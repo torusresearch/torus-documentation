f9f96e1bb39766fb3771204ddb202e7509fa7238
Minh-Phuc Tran <25026967+phuctm97@users.noreply.github.com>
2021-03-11 19:20:06 +0700

    Update product stack

--- src/components/product-stack/index.js
+++ src/components/product-stack/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import styles from "./styles.module.css";
 
 export default function ProductStack() {
-  return <div className={styles.card}></div>;
+  return <div className={styles.card}>Product stack (WIP)</div>;
 }
